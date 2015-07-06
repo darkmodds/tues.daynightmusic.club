a7504538dcd5a9e1a1ed323bfc81d83ad3ccec82
browncardigan <jonathan.nail@gmail.com>
2015-07-06 13:22:28 +1000

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Tuesday Night Music Club</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="daily m00ds, most days" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700">
+<link rel="stylesheet" href="css/tnmc.css" />
+</head>
+<body>
+	
+<h1>#TNMC</h1>
+
+<section>
+
+
+
+</section>
+
+<footer>&copy; 2013-<?= date("Y"); ?></footer>
+
+</body>
+</html>
